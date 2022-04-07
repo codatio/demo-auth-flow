46b37c146db018982247773e7cefb38aeb31f0df
hannasukharieva <hsukharieva@gmail.com>
2022-04-07 12:34:44 +0100

    Initial app setup

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
