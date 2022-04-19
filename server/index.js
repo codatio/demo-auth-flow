83ba4a06c01efadab0011e4a6a5b63d76aa29cfd
hannasukharieva <hsukharieva@gmail.com>
2022-04-19 08:59:07 +0100

    Sets up login API
    - A POST /login endpoint, body { username: string }
    - The API should respond with a new session ID as a GUID (an ID per
    session)
    - Store the user name and their session ID

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
+const { LocalStorage } = require("node-localstorage");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+const userIdMap = new LocalStorage("./bin/users");
+
+app.post('/login', (req, res) => {
+  const userId = uuid.v4()
+  const userName = req.body.username
+
+  userIdMap.setItem(userName, userId)
+  res.json({ userId })
 });
 
 app.listen(PORT, () => {
