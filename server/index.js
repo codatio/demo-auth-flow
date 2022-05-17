e95f6621bb7168f36bc5e6f45ca10665bbb6b314
Lucy Mair <l.mair@codat.io>
2022-05-17 15:21:48 +0100

    Create company on login - first draft

--- server/index.js
+++ server/index.js
@@ -2,22 +2,70 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const uuid = require("uuid");
 const { LocalStorage } = require("node-localstorage");
+const fetch = require("node-fetch");
 
 const PORT = process.env.PORT || 3001;
+const apiKey = process.env.CODAT_API_KEY;
+const codatBaseUrl = "https://api.codat.io";
+
+const apiKeyBase64Encoded = Buffer.from(apiKey).toString("base64");
+const authorisationHeader = "Basic " + apiKeyBase64Encoded;
+
+if (!apiKey) {
+  throw new Error("You must have your Codat api key set as the environment variable CODAT_API_KEY");
+}
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// username -> user ID map
 const userIdMap = new LocalStorage("./bin/users");
 
-app.post('/login', (req, res) => {
-  const userId = uuid.v4()
-  const userName = req.body.username
+const getUserStorage = (userId) => {
+  return new LocalStorage(`./bin/${userId}`);
+};
 
-  userIdMap.setItem(userName, userId)
-  res.json({ userName, userId })
+app.post('/login', async (req, res) => {
+  const userName = req.body.username;
+  
+  if (!userName) {
+    res.status(400).send({error: "username is a required field"});
+  }
+
+  // Try to get user ID for an existing user
+  const userId = userIdMap.getItem(userName) ?? uuid.v4();
+  // Set the userIdMap value
+  userIdMap.setItem(userName, userId);
+
+  const userStorage = getUserStorage(userId);
+  if(!userStorage.getItem("codat-company-id")) {
+    // Codat company has not been created
+    // So we will create it by posting to the API
+    const postResult = await fetch(
+      `${codatBaseUrl}/companies`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: authorisationHeader
+        },
+        body: JSON.stringify({
+          name: userName
+        })
+      }
+    );
+    const resultBody = await postResult.json();
+    const codatCompanyId = resultBody.id;
+    if (codatCompanyId) {
+      console.log("Setting codat company ID as", codatCompanyId, "for user", userId);
+      userStorage.setItem("codat-company-id", codatCompanyId);
+    }
+  }
+
+  res.json({ userName, userId });
 });
 
 app.listen(PORT, () => {
---
+++ server/readme.md
@@ -0,0 +1,6 @@
+# Link demo server
+
+To set up:
+1. `npm install`
+2. Set environment variable CODAT_API_KEY to your Codat API key which can be found here https://app.codat.io/profile
+3. `npm start`
