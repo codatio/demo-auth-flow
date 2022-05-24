48c5ee454ce52d1bd55d6e7863abb9b7321e0b0c
Lucy Mair <l.mair@codat.io>
2022-05-24 12:58:52 +0100

    Extract users controller

--- server/index.js
+++ server/index.js
@@ -1,48 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const uuid = require("uuid");
-const { LocalStorage } = require("node-localstorage");
-const { createCompany } = require("./clients/codat");
 const { config } = require("./config");
+const { userLogin } = require("./controllers/user");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// username -> user ID map
-const userIdMap = new LocalStorage("./bin/users");
-
-const getUserStorage = (userId) => {
-  return new LocalStorage(`./bin/${userId}`);
-};
-
-app.post('/login', async (req, res) => {
-  const userName = req.body.username;
-  
-  if (!userName) {
-    res.status(400).send({error: "username is a required field"});
-  }
-
-  // Try to get user ID for an existing user
-  const userId = userIdMap.getItem(userName) ?? uuid.v4();
-  // Set the userIdMap value
-  userIdMap.setItem(userName, userId);
-
-  const userStorage = getUserStorage(userId);
-  if(!userStorage.getItem("codat-company-id")) {
-    // Codat company has not been created
-    // So we will create it by posting to the Codat API
-    const resultBody = await createCompany(userName);
-    const codatCompanyId = resultBody.id;
-    if (codatCompanyId) {
-      console.log("Setting codat company ID as", codatCompanyId, "for user", userId);
-      userStorage.setItem("codat-company-id", codatCompanyId);
-    }
-  }
-
-  res.json({ userName, userId });
-});
+app.post('/login', userLogin);
 
 app.listen(config.port, () => {
   console.log(`Server listening on ${config.port}`);
