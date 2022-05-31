7c06ec7598c66b6e65a4ed12555c9c0eb5a093f1
hannasukharieva <hsukharieva@gmail.com>
2022-05-31 14:58:37 +0100

    basic setup for data connection display

--- client/src/link-service.js
+++ client/src/link-service.js
@@ -1,15 +1,22 @@
 const linkService = {
-  login: (userName) => (
-    fetch("/login", {
-      method: "POST",
+  login: (userName) =>
+    fetch('/login', {
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ username: userName }),
-    })
-      .then((res) => res.json())
-  )
-}
+    }).then((res) => res.json()),
+  // Get all company connections
+  connections: (userId) =>
+    fetch(`/user/${userId}/connections`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => res.json()),
+};
 
-export { linkService }
+export { linkService };
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,42 +1,87 @@
 import Header from '../../components/Header/Header';
-import {
-  Typography
-} from "@mui/material";
-import "./Dashboard.css";
+import { Typography } from '@mui/material';
+import './Dashboard.css';
 import FlexColumns from '../../components/FlexColumns/FlexColumns';
 import { useParams } from 'react-router-dom';
+import { Fragment, useEffect, useState } from 'react';
+import { linkService } from '../../link-service';
 
 const Dashboard = () => {
   const { userId } = useParams();
+  const [companyConnections, setCompanyConnections] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const listItems = [
     {
-      key: "Name:",
+      key: 'Name:',
       value: <Typography variant="body1">User name</Typography>,
     },
     {
-      key: "User e-mail:",
-      value: <Typography variant="body1">email@company.com</Typography>
+      key: 'User e-mail:',
+      value: <Typography variant="body1">email@company.com</Typography>,
     },
     {
-      key: "User ID:",
-      value: <Typography variant="body1">{userId}</Typography>
+      key: 'User ID:',
+      value: <Typography variant="body1">{userId}</Typography>,
+    },
+  ];
+
+  useEffect(() => {
+    linkService.connections(userId)
+      .then((data) => {
+        setCompanyConnections(data);
+      })
+      .catch(() => {setErrorMessage('Something went wrong.')})
+  }, []);
+
+  const formattedCompanyConnections = companyConnections.map(
+    (connectionObject) => {
+      return {
+        title: connectionObject.platformName,
+        id: connectionObject.id,
+        listItems: [
+          {
+            key: 'Status',
+            value: connectionObject.status,
+          },
+          {
+            key: 'Source type',
+            value: connectionObject.sourceType,
+          },
+        ],
+      };
     }
-  ]
+  );
 
   return (
     <>
-      <Header/>
+      <Header />
       <div className="dashboard-content-wrapper">
         <div>
-          <Typography variant='h3' gutterBottom>
+          <Typography variant="h3" gutterBottom>
             Your account
           </Typography>
-          <FlexColumns listItems={listItems}/>
+          <FlexColumns listItems={listItems} />
+          <Typography variant="h3" gutterBottom>
+            Your connections
+          </Typography>
+          {errorMessage && <Typography>{errorMessage}</Typography>}
+          {formattedCompanyConnections.length > 0 ? (
+            formattedCompanyConnections.map((item) => {
+              return (
+                <Fragment key={item.id}>
+                  <Typography>{item.title}</Typography>
+                  <FlexColumns listItems={item.listItems} />
+                </Fragment>
+              );
+            })
+          ) : (
+            <Typography>Connections not found!</Typography>
+          )}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Dashboard;
