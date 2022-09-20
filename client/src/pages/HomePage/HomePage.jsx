6ab0067493ae3312df024c7eeb1cdea40cc30772
hannasukharieva <hsukharieva@gmail.com>
2022-09-20 15:34:16 +0100

    Add, position, and animate home page blob

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -11,7 +11,8 @@ import './HomePage.css';
 
 //Assets
 import ListIcon from '../../assets/images/icons/bullet-point.svg';
-import Blob from '../../assets/images/blob.svg';
+import Card from '../../assets/images/card.svg';
+import vectors from './vectors'
 
 const HomePage = () => {
   const sellingPoints = [
@@ -23,32 +24,50 @@ const HomePage = () => {
 
   return (
     <>
-      <Header />
-      <div className="home-page-wrapper">
-        <div className="home-page-left-column">
-          <Typography variant="h4" component="h2">Apply for a loan within minutes</Typography>
-          <Typography className="home-page-description">
-            You need money. We have money. It's that simple.
-          </Typography>
+      <div className="home-page-element">
+        <Header className="home-page-header" />
+        <div className="home-page-wrapper">
+          <div className="home-page-left-column">
+            <Typography variant="h4" component="h2">
+              Apply for a loan within minutes
+            </Typography>
+            <Typography className="home-page-description">
+              You need money. We have money. It's that simple.
+            </Typography>
 
-          <List>
-            {sellingPoints.map((sellingPoint) => {
-              return (
-                <ListItem>
-                  <ListItemIcon>
-                    <img src={ListIcon} alt="Checkmark list icon." />
-                  </ListItemIcon>
-                  <ListItemText className="home-page-selling-point" primary={sellingPoint}></ListItemText>
-                </ListItem>
-              );
-            })}
-          </List>
-          <Button variant="contained" color="primary">
-          Apply for a loan
-        </Button>
-        </div>
-        <div className="home-page-right-column">
-          <img src={Blob} alt="An image showing a credit card." />
+            <List>
+              {sellingPoints.map((sellingPoint) => {
+                return (
+                  <ListItem>
+                    <ListItemIcon>
+                      <img src={ListIcon} alt="Checkmark list icon." />
+                    </ListItemIcon>
+                    <ListItemText
+                      className="home-page-selling-point"
+                      primary={sellingPoint}
+                    ></ListItemText>
+                  </ListItem>
+                );
+              })}
+            </List>
+            <Button variant="contained" color="primary">
+              Apply for a loan
+            </Button>
+          </div>
+          <div className="home-page-right-column">
+            <div className="home-page-image">
+              <img
+                className="home-page-card-image"
+                src={Card}
+                alt="An image showing a credit card."
+              />
+              <div className="home-page-vectors-wrapper">
+                {vectors.map((vector) => (
+                  <img src={vector} alt="Decorative vector." />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
