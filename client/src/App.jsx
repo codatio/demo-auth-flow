bee2c62eec902d6f034d474fbe3233a64f6b1811
hannasukharieva <hsukharieva@gmail.com>
2022-09-25 18:00:24 +0100

    Remove login page and references

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 import LoanForm from './pages/LoanForm/LoanForm';
 import HomePage from './pages/HomePage/HomePage';
@@ -12,7 +11,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path={routes.home} element={<HomePage />} />
-        <Route path={routes.login} element={<Login />} />
         <Route path={routes.dashboard(':userId')} element={<Dashboard />} />
         <Route path={routes.loanForm(':userId')} element={<LoanForm />}/>
         <Route path={routes.redirect} element={<RedirectHandler />}/>
