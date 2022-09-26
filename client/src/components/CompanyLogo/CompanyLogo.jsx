22c5714638d9a5e0b50905f86d36a2bf9c0f30c6
hannasukharieva <hsukharieva@gmail.com>
2022-09-26 08:50:15 +0100

    Add link to home page to logo

--- client/src/components/CompanyLogo/CompanyLogo.jsx
+++ client/src/components/CompanyLogo/CompanyLogo.jsx
@@ -1,10 +1,18 @@
-import CompanyLogoImage from '../../assets/images/copay-logo.png'
-import './CompanyLogo.css'
+import CompanyLogoImage from '../../assets/images/copay-logo.png';
+import './CompanyLogo.css';
+import { Link } from 'react-router-dom';
+import { routes } from '../../routes';
 
 const CompanyLogo = () => {
   return (
-    <img className="company-logo" alt="copay-company-logo" src={CompanyLogoImage}/>
-  )
-}
+    <Link to={routes.home}>
+      <img
+        className="company-logo"
+        alt="copay-company-logo"
+        src={CompanyLogoImage}
+      />
+    </Link>
+  );
+};
 
 export default CompanyLogo;
